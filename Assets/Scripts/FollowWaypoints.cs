0b715bfcb13f35440cd3eb27ee48ede14f4bf245
Alejandro Costas <alecostaskb@hotmail.com>
2025-01-06 17:34:24 +0100

    From Waypoints to NavMesh

--- Assets/Scripts/FollowWaypoints.cs
+++ Assets/Scripts/FollowWaypoints.cs
@@ -1,84 +1,40 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowWaypoints : MonoBehaviour
 {
-    private Transform goal;
-    private float speed = 5.0f;
-    private float accuracy = 5.0f;
-    private float rotationSpeed = 2.0f;
+    public GameObject wpManager;
     private GameObject[] waitPoints;
     private GameObject currentNode;
-    private int currentWP = 0;
-    private Graph graph;
 
-    public GameObject wpManager;
+    private NavMeshAgent navAgent;
 
     private void Start()
     {
         Time.timeScale = 5.0f;
         waitPoints = wpManager.GetComponent<WPManager>().waypoints;
-        graph = wpManager.GetComponent<WPManager>().graph;
         currentNode = waitPoints[0];
-    }
 
-    private void LateUpdate()
-    {
-        if (graph.pathList.Count == 0 || currentWP == graph.pathList.Count)
-        {
-            return;
-        }
-
-        currentNode = graph.getPathPoint(currentWP);
-
-        if (Vector3.Distance(graph.pathList[currentWP].getID().transform.position, transform.position) < accuracy)
-        {
-            currentWP++;
-        }
-
-        if (currentWP < graph.pathList.Count)
-        {
-            goal = graph.pathList[currentWP].getID().transform;
-            Vector3 lookAtGoal = new Vector3(
-                goal.position.x,
-                transform.position.y,
-                goal.position.z);
-
-            Vector3 direction = lookAtGoal - transform.position;
-
-            transform.rotation = Quaternion.Slerp(
-                transform.rotation,
-                Quaternion.LookRotation(direction),
-                Time.deltaTime * rotationSpeed);
-
-            transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
-        }
-    }
-
-    public void GotoHeli()
-    {
-        graph.AStar(currentNode, waitPoints[0]);
-
-        currentWP = 0;
+        navAgent = GetComponent<NavMeshAgent>();
     }
 
     public void GotoRuin()
     {
-        graph.AStar(currentNode, waitPoints[7]);
-
-        currentWP = 0;
+        navAgent.SetDestination(waitPoints[1].transform.position);
     }
 
     public void GotoRock()
     {
-        graph.AStar(currentNode, waitPoints[1]);
+        navAgent.SetDestination(waitPoints[3].transform.position);
+    }
 
-        currentWP = 0;
+    public void GotoHeli()
+    {
+        navAgent.SetDestination(waitPoints[2].transform.position);
     }
 
     public void GotoFactory()
     {
-        graph.AStar(currentNode, waitPoints[4]);
-
-        currentWP = 0;
+        navAgent.SetDestination(waitPoints[0].transform.position);
     }
 }
